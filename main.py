588aa02ee307c6b7bfa8ec0dfb040412c7ce63b5
marzzuki <mwmma5000@gmail.com>
2025-09-21 12:52:36 +0300

    feat: first request

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import os
 from fastapi import FastAPI
-from routes import base
-
 app = FastAPI()
 
-app.include_router(base.base_router)
+@app.get("/welcome")
+def welcome():
+    return {
+        "message": "Welcome to the mini-rag API"
+    }
