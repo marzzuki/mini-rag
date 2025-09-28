fca6b8af2edf1ac71a051b1d47fe09ff5545940a
marzzuki <mwmma5000@gmail.com>
2025-09-28 15:57:34 +0300

    refactor: Remove duplicate app generation client and embeddings

--- src/main.py
+++ src/main.py
@@ -15,20 +15,7 @@ async def startup_db_client():
     app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
 
     llm_provider_factory = LLMProviderFactory(settings)
-    app.generation_client = llm_provider_factory.create(
-        provider=settings.GENERATION_BACKEND
-    )
-    app.generation_client.set_generation_model(model_id=settings.GENERATION_MODEL_ID)
 
-    app.embedding_client = llm_provider_factory.create(
-        provider=settings.EMBEDDING_BACKEND
-    )
-    app.embedding_client.set_embedding_model(
-        model_id=settings.EMBEDDING_MODEL_ID,
-        embedding_size=settings.EMBEDDING_MODEL_SIZE,
-    )
-
-    llm_provider_factory = LLMProviderFactory(settings)
     app.generation_client = llm_provider_factory.create(
         provider=settings.GENERATION_BACKEND
     )
