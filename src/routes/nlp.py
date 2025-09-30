ea473b5018bfec91d2e3e79f0e19de0ddd2c51c9
marzzuki <mwmma5000@gmail.com>
2025-09-30 11:32:40 +0300

    fix: Added template parser to all NLPController calls

--- src/routes/nlp.py
+++ src/routes/nlp.py
@@ -28,6 +28,7 @@ async def index_project(request: Request, project_id: str, push_request: PushReq
         vectordb_client=request.app.vectordb_client,
         generation_client=request.app.generation_client,
         embedding_client=request.app.embedding_client,
+        template_parser=request.app.template_parser,
     )
 
     has_records = True
@@ -82,6 +83,7 @@ async def get_index_info(request: Request, project_id: str):
         vectordb_client=request.app.vectordb_client,
         generation_client=request.app.generation_client,
         embedding_client=request.app.embedding_client,
+        template_parser=request.app.template_parser,
     )
 
     collection_info = nlp_controller.get_vector_db_collection_info(project=project)
